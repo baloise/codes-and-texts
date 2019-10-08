ad81905c8c368a92f14e2c41a06880833f2afaee
Michael Mühlebach <michael@anduin.ch>
2019-10-08 21:25:07 +0200

    Add generated spring boot application

---
+++ src/main/java/ch/basler/cut/CodesAndTextsApplication.java
@@ -0,0 +1,13 @@
+package ch.basler.cut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesAndTextsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodesAndTextsApplication.class, args);
+    }
+
+}
