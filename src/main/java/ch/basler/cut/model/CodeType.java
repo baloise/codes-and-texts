76dcfa6cac163d576e0b56b442237f79c8a2628c
Michael Mühlebach <michael@anduin.ch>
2019-10-09 11:52:38 +0200

    Fix typo in view name

--- src/main/java/ch/basler/cut/model/CodeType.java
+++ src/main/java/ch/basler/cut/model/CodeType.java
@@ -4,7 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity(name = "CODETYP")
+@Entity(name = "CODETYPE")
 public class CodeType {
     @Id
     @Column(name = "ID")
