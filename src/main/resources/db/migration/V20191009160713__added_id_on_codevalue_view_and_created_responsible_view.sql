dcf44382b55168a95868f3788f07245b053ad668
Michael Mühlebach <michael@anduin.ch>
2019-10-09 17:53:30 +0200

    Fix missing drop view

--- src/main/resources/db/migration/V20191009160713__added_id_on_codevalue_view_and_created_responsible_view.sql
+++ src/main/resources/db/migration/V20191009160713__added_id_on_codevalue_view_and_created_responsible_view.sql
@@ -1,3 +1,5 @@
+DROP VIEW codevalue;
+
 CREATE VIEW codevalue (id, value, codetype_id, name, creator, created) AS
 -- SELECT DENSE_RANK() OVER (order by codetyp_nr, wert) as id, -- will enumerate - not good for deletes
 SELECT concat_ws(':', codetyp_nr, wert),
