e0ed7db27f3a3157e56cebcba7c074715e971429
Michael Muehlebach <michael@anduin.ch>
2019-10-25 15:46:39 +0200

    Use correct find method signature for pageable service methods

--- cat-server/src/main/java/ch/basler/cat/services/ResponsibleRepository.java
+++ cat-server/src/main/java/ch/basler/cat/services/ResponsibleRepository.java
@@ -2,6 +2,8 @@ package ch.basler.cat.services;
 
 
 import ch.basler.cat.model.Responsible;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -13,6 +15,6 @@ import java.util.List;
 @RepositoryRestResource(collectionResourceRel = "responsible", path = "responsible")
 public interface ResponsibleRepository extends PagingAndSortingRepository<Responsible, Long> {
 
-    List<Responsible> findByPrefix(@Param("prefix") String prefix);
+    Page<List<Responsible>> findByPrefix(@Param("prefix") String prefix, Pageable pageable);
 
 }
