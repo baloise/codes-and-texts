7662183e749541f98953ccc80797ee543d65177a
Michael Muehlebach <michael@anduin.ch>
2019-11-22 14:33:09 +0100

    Add license header to all java source files

--- cat-server/src/main/java/ch/basler/cat/services/CodeTypeRepository.java
+++ cat-server/src/main/java/ch/basler/cat/services/CodeTypeRepository.java
@@ -1,5 +1,19 @@
+/*
+ * Copyright 2019 Baloise Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ch.basler.cat.services;
-
 
 import ch.basler.cat.model.CodeType;
 import ch.basler.cat.model.InlineCodeValue;
@@ -13,7 +27,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
 @CrossOrigin
-@RepositoryRestResource(collectionResourceRel = "codeType", path = "codeType", excerptProjection = InlineCodeValue.class)
+@RepositoryRestResource(
+        collectionResourceRel = "codeType",
+        path = "codeType",
+        excerptProjection = InlineCodeValue.class)
 public interface CodeTypeRepository extends PagingAndSortingRepository<CodeType, Long> {
 
     Page<List<CodeType>> findByName(@Param("name") String name, Pageable pageable);
