51be9a6a269d7aa44d0d7613692c63733bed4f61
Michael Mühlebach <michael@anduin.ch>
2020-04-06 17:11:20 +0200

    Remove unused import

--- cat-server/src/main/java/ch/basler/cat/controller/CodeTypeController.java
+++ cat-server/src/main/java/ch/basler/cat/controller/CodeTypeController.java
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @RestController
 public class CodeTypeController {
