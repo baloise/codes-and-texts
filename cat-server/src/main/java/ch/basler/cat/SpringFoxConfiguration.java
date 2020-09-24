7a6fb039f3e62f6b890fd8517ccd9bfbaf6ba99f
Michael Mühlebach <michael@anduin.ch>
2020-09-24 10:02:42 +0200

    Enable OpenAPI and Swagger UI

---
+++ cat-server/src/main/java/ch/basler/cat/SpringFoxConfiguration.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2020 Baloise Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ch.basler.cat;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
+
+import java.util.Collections;
+
+@Configuration
+public class SpringFoxConfiguration {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Code and Texts REST API",
+                "Management System for I18N codes and texts.",
+                "0.0.1",
+                "Terms of service",
+                new Contact("John Doe", "www.example.com", "myeaddress@company.com"),
+                "Apache 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ch.basler.cat.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * SwaggerUI information
+     */
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
+                .build();
+    }
+}
